0d60723bbc3e6c587c19bd4ee3cca5d30ef09e14
Marcus Valtonen Örnhag <marcusvaltonen@gmail.com>
2021-04-27 16:34:11 +0200

    Add rEr solver fron DronePoseLib main (#4)
    * Add bindings
    
    * add tests and compile

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -79,6 +79,42 @@ py::list get_valtonenornhag_arxiv_2021_frEfr_wrapper(const Eigen::Ref<const Eige
     return lst;
 }
 
+py::list get_valtonenornhag_arxiv_2021_rEr_wrapper(const Eigen::Ref<const Eigen::MatrixXd> &x1,
+                                                   const Eigen::Ref<const Eigen::MatrixXd> &x2,
+                                                   const Eigen::Ref<const Eigen::MatrixXd> &R1,
+                                                   const Eigen::Ref<const Eigen::MatrixXd> &R2,
+                                                   const double focal_length)
+{
+
+    if (x1.rows() != 2 || x1.cols() != 3) {
+        throw std::invalid_argument("First argument should be of size 2x3");
+    }
+    if (x2.rows() != 2 || x2.cols() != 3) {
+        throw std::invalid_argument("Second argument should be of size 2x3");
+    }
+    if (R1.rows() != 3 || R1.cols() != 3) {
+        throw std::invalid_argument("Third argument should be of size 3x3");
+    }
+    if (R2.rows() != 3 || R2.cols() != 3) {
+        throw std::invalid_argument("Fourth argument should be of size 3x3");
+    }
+
+    std::vector<DronePoseLib::RelPose> posedata =
+        DronePoseLib::ValtonenOrnhagArxiv2021Extra::get_rEr(x1, x2, R1, R2, focal_length);
+
+    py::list lst;
+    for (int i = 0; i < posedata.size(); i++) {
+        py::dict d;
+        d["F"] = posedata[i].F;
+        d["f"] = posedata[i].f;
+        d["r"] = posedata[i].r;
+        d["t"] = posedata[i].t;
+        lst.append(d);
+    }
+
+    return lst;
+}
+
 
 PYBIND11_MODULE(droneposelib, m) {
   m.doc() = R"doc(
@@ -90,6 +126,7 @@ PYBIND11_MODULE(droneposelib, m) {
 
            get_valtonenornhag_arxiv_2021_fEf
            get_valtonenornhag_arxiv_2021_frEfr
+           get_valtonenornhag_arxiv_2021_rEr
     )doc";
 
   m.def("get_valtonenornhag_arxiv_2021_fEf", &get_valtonenornhag_arxiv_2021_fEf_wrapper, R"doc(
@@ -100,6 +137,12 @@ PYBIND11_MODULE(droneposelib, m) {
 
   m.def("get_valtonenornhag_arxiv_2021_frEfr", &get_valtonenornhag_arxiv_2021_frEfr_wrapper, R"doc(
         Valtonen Ornhag et al. (ArXiV, 2021) 4-point radial relative pose using IMU data.
+
+        More info to be added...
+    )doc");
+
+  m.def("get_valtonenornhag_arxiv_2021_rEr", &get_valtonenornhag_arxiv_2021_rEr_wrapper, R"doc(
+        Valtonen Ornhag et al. (ArXiV extra, 2021) 3-point radial relative pose using IMU data.
 
         More info to be added...
     )doc");
